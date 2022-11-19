36b0b74da41e2a54536c16b33f3ceb176abf40f2
Scott <scfurl@users.noreply.github.com>
2022-11-18 20:40:31 -0800

    reallyworking

--- src/mergebams.rs
+++ src/mergebams.rs
@@ -10,25 +10,34 @@ head /home/sfurlan/scratch/MRB2/ITS_D544_3/outs/per_sample_outs/ITS_D544_3/count
 
 cd ~/develop/mergebams/test
 cargo build --release
-../target/release/mergebams -t 4 -i bam1.bam,bam2.bam -l test1_,test2_ -b dummy -o .
+../target/release/mergebams -i bam1.bam,bam2.bam -l test1_,test2_ -b barcodes1.tsv.gz,barcodes2.tsv.gz -o .
+zcat < out_barcodes.tsv.gz
 samtools view out_bam.bam | head -n 200
+samtools sort out_bam.bam > out_bam.sorted.bam
+samtools view out_bam.sorted.bam | head -n 200
 **/
+
+
 extern crate simple_log;
 extern crate clap;
 extern crate bam;
-// extern crate parasailors;
+extern crate csv;
+extern crate flate2;
 
-// use simple_log::LogConfigBuilder;
-
+use flate2::GzBuilder;
+use flate2::Compression;
 use bam::RecordWriter;
 use bam::record::tags::TagValue;
 use clap::{App, load_yaml};
-
 use std::str;
-use std::io;
-use std::io::BufWriter;
-
-// #[derive(Clone)]
+use flate2::{read};
+use std::{
+    // error::Error,
+    ffi::OsStr,
+    fs::File,
+    io::{self, BufReader, BufRead, Write},
+    path::Path
+};
 
 
 struct Params {
@@ -40,13 +49,22 @@ struct Params {
 }
 
 
+fn main() {
+    let params = load_params();
+    let params = addtags(params);
+    let _result = lines_from_file(params);
+    return;
+    
+}
+
+
 fn load_params() -> Params {
     let yaml = load_yaml!("params_mergebams.yml");
     let params = App::from_yaml(yaml).get_matches();
     let inputs = params.value_of("inputs").unwrap();
     let labels = params.value_of("labels").unwrap();
     let threads: usize = params.value_of("threads").unwrap_or("1").parse().unwrap();
-    let bcs = params.value_of("bcs").unwrap_or("1");
+    let bcs = params.value_of("bcs").unwrap_or("0");
     let out = params.value_of("out").unwrap();
     Params{
         inputs: inputs.to_string(),
@@ -57,17 +75,42 @@ fn load_params() -> Params {
     }
 }
 
-fn main() {
-    let params = load_params();
-    addtags(params);
-    // combinebcs(&params);
-    return;
-    
+
+fn lines_from_file(params: Params) -> io::Result<()>{
+    let out_csv = params.out.to_string()+"/out_barcodes.tsv.gz";
+    let s1 = params.bcs.to_string();
+    let tsvvec = s1.split(",").collect::<Vec<&str>>();
+    let labvec1 = params.labels.to_string();
+    let labvec = labvec1.split(",").collect::<Vec<&str>>();
+    let f = File::create(out_csv)?;
+    let mut gz = GzBuilder::new()
+                    .filename("/out_barcodes.tsv.gz")
+                    .write(f, Compression::default());
+
+    for (pos, filename) in tsvvec.iter().enumerate() {
+        let path = Path::new(filename);
+        let file = match File::open(&path) {
+            Err(_why) => panic!("couldn't open {}", path.display()),
+            Ok(file) => file,
+        };
+        if path.extension() == Some(OsStr::new("gz")){
+            let buf = BufReader::new(read::GzDecoder::new(file));
+            for line in buf.lines(){
+                writeln!(&mut gz, "{}", labvec[pos].to_string() + &line.unwrap())?;
+            }
+        }else{
+            let buf = BufReader::new(file);
+            for line in buf.lines(){
+                writeln!(&mut gz, "{}", labvec[pos].to_string() + &line.unwrap())?;
+            }
+        }
+    }
+    return Ok(());
 }
 
 
 
-fn addtags(params: Params) {
+fn addtags(params: Params) -> Params{
     let out_bam = params.out.to_string()+"/out_bam.bam";
     let s1 = params.inputs.to_string();
     let bamvec = s1.split(",").collect::<Vec<&str>>();
@@ -93,9 +136,9 @@ fn addtags(params: Params) {
                     let labstr = labvec[pos];
                     let preftag = labstr.as_bytes().to_vec();
                     let oldtag = array_view.to_vec();
-                    let newCB = &[preftag, oldtag].concat();
+                    let new_cb = &[preftag, oldtag].concat();
                     newrecord.tags_mut().remove(b"CB");
-                    newrecord.tags_mut().push_string(b"CB", &newCB);
+                    newrecord.tags_mut().push_string(b"CB", &new_cb);
                     writer.write(&newrecord).unwrap();
                 },
                 Some(TagValue::Char(value)) => println!("Char = {}", value),
@@ -103,6 +146,7 @@ fn addtags(params: Params) {
             }
         }
     }
+    return params;
 }
     
 
