86178a0b2713b547db503770ffffd92a1f1d9980
Scott <scfurl@users.noreply.github.com>
2022-11-20 20:55:16 -0800

    checksq1

--- src/mergebams.rs
+++ src/mergebams.rs
@@ -8,6 +8,10 @@ cargo build --release
 ml SAMtools
 samtools view -h -s 0.0001 /home/sfurlan/scratch/MRB2/ITS_D383_3/outs/per_sample_outs/ITS_D383_3/count/sample_alignments.bam > ~/develop/mergebams/test/bam1.bam
 samtools view -h -s 0.0001 /home/sfurlan/scratch/MRB2/ITS_D544_3/outs/per_sample_outs/ITS_D544_3/count/sample_alignments.bam > ~/develop/mergebams/test/bam2.bam
+samtools view -h -s 0.0001 /home/sfurlan/scratch/MRB2/ITS_D544_3/outs/per_sample_outs/ITS_D544_3/count/sample_alignments.bam > ~/develop/mergebams/test/bam3.bam
+samtools view -h -s 0.0001 /home/sfurlan/scratch/NB1/AML_601_34/outs/per_sample_outs/AML_601_34/count/sample_alignments.bam > ~/develop/mergebams/test/bam4.bam
+
+
 head /home/sfurlan/scratch/MRB2/ITS_D544_3/outs/per_sample_outs/ITS_D544_3/count/sample_barcodes.csv
 
 ************************************
@@ -39,7 +43,9 @@ extern crate clap;
 extern crate bam;
 extern crate csv;
 extern crate flate2;
+extern crate differ;
 
+use differ::{Differ, Tag};
 use flate2::GzBuilder;
 use flate2::Compression;
 use bam::RecordWriter;
@@ -52,7 +58,7 @@ use std::{
     ffi::OsStr,
     fs::File,
     io::{self, BufReader, BufRead, Write},
-    path::Path
+    path::Path,
 };
 
 
@@ -67,10 +73,14 @@ struct Params {
 
 fn main() {
     let params = load_params();
+    let params = checkheaders(params);
     let params = addtags(params);
-    let _result = lines_from_file(params);
-    return;
-    
+    if params.bcs == "0"{
+        return;
+    } else {
+        let _result = lines_from_file(params);
+        return;
+    }
 }
 
 
@@ -124,32 +134,68 @@ fn lines_from_file(params: Params) -> io::Result<()>{
     return Ok(());
 }
 
+// fn print_lines(c: char, lines: &[&str]) {
+//     for line in lines {
+//         println!("{} {}", c, line);
+//     }
+// }
 
+fn checkheaders(params: Params) -> Params{
+    let inputs = params.inputs.to_string();
+    let bam_vec = inputs.split(",").collect::<Vec<&str>>();
+    let mut header_vec = Vec::new();
+    for inbam in bam_vec.iter() {
+        let hreader = bam::BamReader::from_path(inbam, 0).unwrap();
+        let header = hreader.header().clone();
+        header_vec.push(header);
+    }
+    // let result = checkheaders(header_vec);
+    // for header in header_vec.iter() {
+    //     println!("{:?}", header.reference_names())
+    // }
+    let a = header_vec[0].reference_names();
+    let b = header_vec[1].reference_names();
+    let differ = Differ::new(&a, &b);
+    for span in differ.spans() {
+        match span.tag {
+            Tag::Equal => println!("all headers are equal!"),
+            Tag::Insert => println!("{:?}", span),
+            Tag::Delete => println!("{:?}", span),
+            Tag::Replace => println!("{:?}", span),
+        }
+    }
+    return params;
+}
+
+// fn checkheaders(header_vec: Vec<bam::Header>) -> io::Result<()>{
+
+//     return Ok(());
+// }
 
 fn addtags(params: Params) -> Params{
     let out_bam = params.out.to_string()+"/out_bam.bam";
-    let s1 = params.inputs.to_string();
-    let bamvec = s1.split(",").collect::<Vec<&str>>();
-    let labvec1 = params.labels.to_string();
-    let labvec = labvec1.split(",").collect::<Vec<&str>>();
+    let inputs = params.inputs.to_string();
+    let bam_vec = inputs.split(",").collect::<Vec<&str>>();
+    let labels = params.labels.to_string();
+    let lab_vec = labels.split(",").collect::<Vec<&str>>();
     let (read_threads, write_threads) = if (*&params.threads as i8) > 2{
         (((*&params.threads/2) -1) as u16, ((*&params.threads/2) -1) as u16)
     } else {
         (0 as u16, 0 as u16)
     };
-    let hreader = bam::BamReader::from_path(bamvec[0], read_threads).unwrap();
+    let hreader = bam::BamReader::from_path(bam_vec[0], read_threads).unwrap();
     let mut writer = bam::BamWriter::build()
         .write_header(true)
         .additional_threads(write_threads)
         .from_path(out_bam, hreader.header().clone()).unwrap();
-    for (pos, inbam) in bamvec.iter().enumerate() {
+    for (pos, inbam) in bam_vec.iter().enumerate() {
         eprintln!("Opening: {:?}", inbam);
         let reader = bam::BamReader::from_path(inbam.to_string(), read_threads).unwrap();
         for record in reader {
             let mut newrecord = record.as_ref().unwrap().clone();
             match record.unwrap().tags().get(b"CB") {
                 Some(TagValue::String(array_view, _)) => {
-                    let labstr = labvec[pos];
+                    let labstr = lab_vec[pos];
                     let preftag = labstr.as_bytes().to_vec();
                     let oldtag = array_view.to_vec();
                     let new_cb = &[preftag, oldtag].concat();
